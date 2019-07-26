de77949dcdddfbb3a1f43c705c8f8c2734da33ab
jhonatan velez <talimon@outlook.com>
2019-07-26 13:58:50 -0500

    solucion del Dom

--- 1-DOM/index.js
+++ 1-DOM/index.js
@@ -10,3 +10,18 @@ Obtener e imprimir en consola del siguiente HTML los siguientes elementos
 * solo el elemento con clase 'box' que se encuentra dentro del elemento 'wrap' que a su vez esta dentro del elemento 'container'
 
 */
+//todos los divs de la pagina
+let divs = document.getElementsByTagName("div");
+console.log(divs);
+//elementos con id Header
+let header = document.getElementById("header");
+console.log(header);
+//primer elemento con clase box
+let box = document.getElementsByClassName("wrap");
+console.log(box);
+//solo divs con la clase wrap
+let divW = document.querySelectorAll("div.wrap");
+console.log(divW);
+// unico elemento con clase box dentro de wrap dentro de container
+let unico = document.querySelector("div.container > div.wrap > span.box");
+console.log(unico);
